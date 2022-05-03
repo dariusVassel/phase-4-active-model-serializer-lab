91cc08e274e3b976f2837c42f51750bdd052b906
+ <+>
2022-05-03 15:59:04 -0400

    Complete

---
+++ app/serializers/author_post_serializer.rb
@@ -0,0 +1,16 @@
+class AuthorPostSerializer < ActiveModel::Serializer
+  # attributes :title, :short_content
+  # has_many :tags
+
+  # def short_content
+  #   "#{self.object.content[0..39]}..."
+  # end
+
+  attributes :title, :short_content
+  has_many :tags
+
+  def short_content
+    "#{object.content[0..39]}..."
+  end
+
+end
